6fb68f599f8ff290093b283b7bddb32849a4694a
Legaeli <depperschmidtchristian@gmail.com>
2026-09-18 01:09:26 +0200

    Init Commit

---
+++ ConsoleProgram/TestGuardian/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Main()
+{
+    while (true)
+    {
+        string userInput = Console.ReadLine();
+        if (userInput != null)
+        {
+            OutputName(userInput);
+        }
+
+    }
+}
+
+void OutputName(string input)
+{
+    Console.WriteLine(input);
+}
+
+
+
+Main();
